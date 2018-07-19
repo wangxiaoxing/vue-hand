a3ab950513ea1cb559d9a6ce2e37f14b38f4db04
wangxiaoxing <32806692+wangxiaoxing@users.noreply.github.com>
2018-07-19 18:36:05 +0800

    Create cp1.vue

---
+++ src/components/cp1.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="wrapper">
+      <h2>{{msg}}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  components:{},
+  props:{},
+  data(){
+    return {
+        msg:"我是子组件的内容"
+    }
+  },
+  watch:{},
+  computed:{},
+  methods:{},
+  created(){},
+  mounted(){}
+}
+</script>
+<style scoped>
+</style>
